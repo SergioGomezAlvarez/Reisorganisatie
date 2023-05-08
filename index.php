625b586bdeea64d0d1664f9e8de50540494c6137
Osso <a.asamaklil500@GMAIL.COM>
2023-05-08 13:31:38 +0200

    .

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
@@ -24,6 +24,18 @@
 
         </div>
     </div>
+    <div class="logo">
+        <img class="logo-img" src="img/Logo1.2.jpg" alt="">
+    </div>
+    <nav class="navbar">
+        <ul class="navlists">
+            <li class="navlist"><a href="#home">Verblijven</a></li>
+            <li class="navlist"><a href="#restaurants">Vluchten</a></li>
+            <li class="navlist"><a href="#restaurants">Restaurants</a></li>
+            <li class="navlist"><a href="#tours">Popular Destinations</a></li>
+        </ul>
+    </nav>
+
 </header>
 
 </body>
